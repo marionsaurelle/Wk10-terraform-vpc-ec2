f47937948f006d2540fbeb8174d27406dc0f8ef2
saurelle <msmkamdem@gmail.com>
2024-10-06 15:02:47 -0500

    new files modified

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "server1" {
   ami = "ami-01b799c439fd5516a"
-  instance_type = "t2.micro"
+  instance_type = var.INSTANCE_TYPE
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.REGION}a"
   subnet_id = aws_subnet.Private1.id
   user_data = file("code.sh")
   tags = {
-    Name = "webserver-1"
+    Name = var.ENVIRONMENT
   }
 }
 resource "aws_instance" "server2" {
   ami = "ami-01b799c439fd5516a"
-  instance_type = "t2.micro"
+  instance_type = var.INSTANCE_TYPE
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
   availability_zone = "us-east-1b"
   subnet_id = aws_subnet.Private2.id
